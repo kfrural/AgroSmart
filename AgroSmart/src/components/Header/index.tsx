171dda4568ccf6028adff869835e7a19f8fd1227
Karla C. Ferreira <karlinharural@gmail.com>
2025-01-22 22:43:44 +0000

    structuring_components

---
+++ AgroSmart/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from 'react';
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  
+
+  return (
+    
+  );
+};
+
+export default Header;
